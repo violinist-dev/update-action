4f4b4e261c5d9097c344d5d511b5298b8b508e82
Eirik S. Morland <eirik@morland.no>
2024-09-16 20:18:06 +0200

    Try multi line

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
 $json=$(exec docker run -e REPO_TOKEN -e LICENCE_KEY -e USER_TOKEN -e PROJECT_URL ghcr.io/violinist-dev/update-check-runner:$1-multi-composer-$2)
-echo "json=$json" >> $GITHUB_OUTPUT
+echo 'json<<EOF' >> $GITHUB_OUTPUT
+echo $json >> $GITHUB_OUTPUT
+echo 'EOF' >> $GITHUB_OUTPUT
