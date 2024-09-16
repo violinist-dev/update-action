51415d90ee3b3dc9c2fefc8a673f193a14433b6d
Eirik S. Morland <eirik@morland.no>
2024-09-16 20:24:19 +0200

    debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,3 +4,5 @@ exec docker run -e REPO_TOKEN -e LICENCE_KEY -e USER_TOKEN -e PROJECT_URL ghcr.i
 echo 'json<<EOF' >> $GITHUB_OUTPUT
 docker run -e REPO_TOKEN -e LICENCE_KEY -e USER_TOKEN -e PROJECT_URL ghcr.io/violinist-dev/update-check-runner:$1-multi-composer-$2 >> $GITHUB_OUTPUT
 echo 'EOF' >> $GITHUB_OUTPUT
+
+cat $GITHUB_OUTPUT
