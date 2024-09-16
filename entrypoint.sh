d71ecfee9572cc1d7ce795cb9ac289035d45cb57
Eirik S. Morland <eirik@morland.no>
2024-09-16 20:12:21 +0200

    Try to fix output

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-exec docker run -e REPO_TOKEN -e LICENCE_KEY -e USER_TOKEN -e PROJECT_URL ghcr.io/violinist-dev/update-check-runner:$1-multi-composer-$2
+$json=$(exec docker run -e REPO_TOKEN -e LICENCE_KEY -e USER_TOKEN -e PROJECT_URL ghcr.io/violinist-dev/update-check-runner:$1-multi-composer-$2)
+echo "json=$json" >> $GITHUB_OUTPUT
