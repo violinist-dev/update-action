0a05e4c5b030183bd12265dbd65486afc81dce52
Eirik Stanghelle Morland <eirik@morland.no>
2024-09-16 20:06:19 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-exec docker run ghcr.io/violinist-dev/update-check-runner:$1-multi-composer-$2
+exec docker run -e LICENCE_KEY -e USER_TOKEN -e PROJECT_URL ghcr.io/violinist-dev/update-check-runner:$1-multi-composer-$2
