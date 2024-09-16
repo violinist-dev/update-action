cb45d113a5986627fb0bc20b4826bb37b1ce425c
Eirik S. Morland <eirik@morland.no>
2024-09-16 20:54:11 +0200

    clean up

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env bash
 
-echo 'json<<EOF' >> $GITHUB_OUTPUT
+echo 'json<<MY_SUPER_SPECIAL_DELIMETER_I_MEAN_WHO_KNOWS_WHATS_IN_PACKAGE_NAMES' >> $GITHUB_OUTPUT
 docker run -e REPO_TOKEN -e LICENCE_KEY -e USER_TOKEN -e PROJECT_URL ghcr.io/violinist-dev/update-check-runner:$1-multi-composer-$2 >> $GITHUB_OUTPUT
 echo -e "\n" >> $GITHUB_OUTPUT
-echo 'EOF' >> $GITHUB_OUTPUT
-
-set 
-env
-
-cat $GITHUB_OUTPUT
+echo 'MY_SUPER_SPECIAL_DELIMETER_I_MEAN_WHO_KNOWS_WHATS_IN_PACKAGE_NAMES' >> $GITHUB_OUTPUT
